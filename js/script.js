5a6e9f4f8885fae96db002bc1f33a24e476f2663
Erik Tangvik <erikjt@outlook.com>
2020-07-01 14:36:14 -0500

    added a modal

--- js/script.js
+++ js/script.js
@@ -5,3 +5,32 @@ const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
 });
+
+//Modal for Blog Page
+
+// Get DOM Elements
+const modal = document.querySelector("#my-modal");
+const modalBtn = document.querySelector("#modal-btn");
+const closeBtn = document.querySelector(".close");
+
+// Events
+modalBtn.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+window.addEventListener("click", outsideClick);
+
+// Open
+function openModal() {
+  modal.style.display = "block";
+}
+
+// Close
+function closeModal() {
+  modal.style.display = "none";
+}
+
+// Close If Outside Click
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+}
